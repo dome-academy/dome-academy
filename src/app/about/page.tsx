328255df69f918330511ab244e1c2628762ede35
Emeka Allison <emekaallison4@gmail.com>
2023-04-10 15:32:18 +0100

    Feat: About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,36 +11,39 @@ const AboutPage = () => {
       }
     >
       <header className='flex flex-col xl:flex-row divide-y-2 xl:divide-x-2 xl:divide-y-0 divide-neutral-950 dark:divide-slate-50 w-full'>
-        <section className='py-20 px-8 md:px-10 lg:py-40 w-full xl:w-3/5'>
-          <h1
-            className={
-              "text-3xl md:text-4xl font-semibold mb-4 text-slate-900 dark:text-slate-100 " +
-              ClashDisplay.className
-            }
-          >
-            Join the growth community of innovators, developers and
-            entreprenuers
-          </h1>
-          <p className=' text-slate-700 dark:text-slate-300 mt-4  xl:text-lg'>
-            As a member of Dome Academy, you have access to community meetings,
-            mentorship and collaboration with seasoned instructors and experts.
-          </p>
+        <section className='py-20 px-8 md:pl-10 lg:py-40 w-full xl:w-3/5 xl:grid xl:items-center'>
+          <div className='max-w-3xl'>
+            <h1
+              className={
+                "text-3xl md:text-4xl lg:text-5xl font-semibold mb-4 text-slate-800 dark:text-slate-200 xl:w-11/12 " +
+                ClashDisplay.className
+              }
+            >
+              We are growth community of innovators, developers and
+              entreprenuers
+            </h1>
+            <p className=' text-slate-700 dark:text-slate-300 mt-4  xl:text-lg w-3/4'>
+              As a member of Dome Academy, you have access to community
+              meetings, mentorship and collaboration with seasoned instructors
+              and experts.
+            </p>
+          </div>
         </section>
-        <section className='w-full xl:w-2/5 grid grid-cols-2 grid-rows-2 gap-0'>
-          <div className='bg-white/30 h-auto relative aspect-[5/3] xl:aspect-square'>
+        <section className='w-full xl:w-2/5 grid grid-cols-2 grid-rows-2 gap-0 bg-black dark:bg-white'>
+          <div className='bg-white/30 h-auto relative aspect-[5/4] xl:aspect-square'>
             <Image
-              src='/images/guy-with-laptop.webp'
+              src='/images/ds.webp'
               alt='Something'
-              className='object-cover border-b-2 border-r-2 border-neutral-950 dark:border-slate-50'
+              className='object-cover border-b-2 border-r-2 border-neutral-950 dark:border-slate-50 filter brightness-125'
               fill
               sizes='(max-width: 768px) 100vw,
               (max-width: 1200px) 50vw,
               33vw'
             />
           </div>
-          <div className='bg-white/30 h-auto relative aspect-[5/3] xl:aspect-square'>
+          <div className='bg-white/30 h-auto relative aspect-[5/4] xl:aspect-square'>
             <Image
-              src='/images/guy-with-laptop.webp'
+              src='/images/pm.webp'
               alt='Something'
               className='object-cover border-b-2 border-neutral-950 dark:border-slate-50'
               fill
@@ -51,7 +54,7 @@ const AboutPage = () => {
           </div>
           <div className='bg-white/30 h-auto relative col-span-2'>
             <Image
-              src='/images/guy-with-laptop.webp'
+              src='/images/people-working.jpg'
               alt='Something'
               className='object-cover'
               fill
