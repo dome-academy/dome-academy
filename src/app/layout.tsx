841bc3c8f145b83233584989824aecf5ce5c4885
Emeka Allison <emekaallison4@gmail.com>
2023-05-23 20:12:48 +0100

    Added OG properties

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,14 @@ import Navbar from "./navbar";
 export const metadata = {
   title: "Dome Academy | Collaborate together and build great things",
   description: "Connect, Learn, Create and Grow with a community of like minds",
+  openGraph: {
+    title: "Dome Academy | Collaborate together and build great things",
+    description:
+      "Connect, Learn, Create and Grow with a community of like minds",
+    images: ["images/cover.PNG"],
+    siteName: "Dome Academy",
+    url: "https://domeinitiative.com",
+  },
 };
 
 export default function RootLayout({
