84d9c33aa95f7c835900bac4339f5775cc854977
Emeka Allison <emekaallison4@gmail.com>
2023-05-24 08:46:34 +0100

    Meta tags fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "./navbar";
 export const metadata = {
   title: "Dome Academy | Collaborate together and build great things",
   description: "Connect, Learn, Create and Grow with a community of like minds",
+  keywords: "Tech, Learn, Academy, Collaboration, web development",
   openGraph: {
     title: "Dome Academy | Collaborate together and build great things",
     description:
@@ -13,6 +14,15 @@ export const metadata = {
     images: ["images/cover.PNG"],
     siteName: "Dome Academy",
     url: "https://domeinitiative.com",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    description:
+      "Connect, Learn, Create and Grow with a community of like minds",
+    title: "Dome Academy | Collaborate together and build great things",
+    images: ["images/cover.PNG"],
+    site: "@joindomeacademy",
   },
 };
 
