a5600269937cab506fb891256618cb8a4db73e33
Emeka Allison <emekaallison4@gmail.com>
2023-06-07 20:17:03 +0100

    Feat: Webinars details

--- src/pageBundles/Home/WebinersSection.tsx
+++ src/pageBundles/Home/WebinersSection.tsx
@@ -1,5 +1,48 @@
+import WebinarCard, { WebinarCardProps } from "@/components/WebinarCard";
 import { ClashDisplay, ClashGrotesk } from "@/utils/font";
 import React from "react";
+
+const webinars: WebinarCardProps[] = [
+  {
+    coverImage: "/images/webinar-poster.png",
+    date: "April 20th",
+    link: "#",
+    speaker: {
+      image: "/images/man-1.webp",
+      name: "David Lee",
+      position: "Product Designer at Inter",
+    },
+    time: "10am - GMT+1",
+    title: "How to get started with Figma",
+    venue: "google meet",
+  },
+  {
+    coverImage: "/images/webinar-poster.png",
+    date: "April 20th",
+    link: "#",
+    speaker: {
+      image: "/images/man-1.webp",
+      name: "David Lee",
+      position: "Product Designer at Inter",
+    },
+    time: "10am - GMT+1",
+    title: "How to get started with Figma",
+    venue: "google meet",
+  },
+  {
+    coverImage: "/images/webinar-poster.png",
+    date: "April 20th",
+    link: "#",
+    speaker: {
+      image: "/images/man-1.webp",
+      name: "David Lee",
+      position: "Product Designer at Inter",
+    },
+    time: "10am - GMT+1",
+    title: "How to get started with Figma",
+    venue: "google meet",
+  },
+];
 
 const WebinersSection = () => {
   return (
@@ -17,9 +60,14 @@ const WebinersSection = () => {
       >
         Upcoming Live sessions 🎙
       </h1>
-      <p className='text-base md:text-lg max-w-xl mx-auto mb-8 mt-4 text-center text-slate-700 dark:text-slate-300 '>
+      <p className='text-base md:text-lg max-w-xl mx-auto mb-8 xl:mb-12 mt-4 text-center text-slate-700 dark:text-slate-300 '>
         Ignite Your Learning Journey. Join & Engage with Our Upcoming Webinars!
       </p>
+      <article className='grid lg:grid-cols-3 max-w-screen-xl mx-auto lg:gap-x-4 xl:gap-x-6 px-4 md:px-8 lg:px-8'>
+        {webinars.map((web, i) => (
+          <WebinarCard key={i} {...web} />
+        ))}
+      </article>
     </section>
   );
 };
