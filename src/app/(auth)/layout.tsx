09d889291436f34575ee1c98f8c803565060638b
Emeka Allison <emekaallison4@gmail.com>
2023-07-06 04:55:17 +0100

    Feat: Dome Logo on auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
-import { ClashGrotesk } from "@/utils/font";
+import { ClashGrotesk, ClashDisplay } from "@/utils/font";
 import Image from "next/image";
+import Link from "next/link";
 
 export const metadata = {
   title: "Sign in to Dome Academy",
@@ -33,7 +34,28 @@ export default function RootLayout({
     <html lang='en'>
       <body className='bg-gray-50 dark:bg-slate-950 w-screen overflow-x-hidden'>
         <main className={` min-h-screen mb-0 flex ` + ClashGrotesk.className}>
-          <section className='h-screen grid bg-gray-50 dark:bg-neutral-950 w-full lg:w-1/2 xl:w-2/5 text-slate-900 dark:text-slate-200 place-items-center'>
+          <section className='h-screen grid bg-gray-50 dark:bg-neutral-950 w-full lg:w-1/2 xl:w-2/5 text-slate-900 dark:text-slate-200 place-items-center relative'>
+            <nav className='absolute top-4 left-4'>
+              <Link
+                href='/'
+                className='px-4 md:px-6 flex gap-x-0.5 items-center scale-105 h-full py-3'
+              >
+                <Image
+                  src='/images/logo.png'
+                  height={40}
+                  width={40}
+                  alt='logo'
+                />
+                <h1
+                  className={
+                    `text-lg md:text-xl text-gray-900 dark:text-gray-200 font-semibold ` +
+                    ClashDisplay.className
+                  }
+                >
+                  Dome
+                </h1>
+              </Link>
+            </nav>
             {children}
           </section>
           <figure className='hidden lg:block h-screen relative flex-1'>
